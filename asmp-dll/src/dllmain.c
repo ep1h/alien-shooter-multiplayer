b97ee4cc478289cbbc974e52765810aec747ba4a
ep1h <ep1hxx@gmail.com>
2023-02-20 04:03:03 -0800

    Implement entry point and main loop for .dll

---
+++ asmp-dll/src/dllmain.c
@@ -0,0 +1,31 @@
+#include <windows.h>
+
+static HINSTANCE instance_handle = 0;
+
+static void dllmain_entry(void);
+
+static void dllmain_entry(void)
+{
+    while (1)
+    {
+        Sleep(100);
+        if (GetAsyncKeyState(VK_F12))
+        {
+            break;
+        }
+    }
+    FreeLibraryAndExitThread(instance_handle, 0);
+}
+
+BOOL WINAPI DllMain(HMODULE hinst, DWORD reason, LPVOID reserved)
+{
+    (void)reserved;
+    instance_handle = hinst;
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        DWORD thread_id = 0;
+        CloseHandle(CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)dllmain_entry,
+                                 0x0, 0, &thread_id));
+    }
+    return TRUE;
+}

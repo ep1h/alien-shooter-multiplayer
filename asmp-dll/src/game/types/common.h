40aedbff55af5a88cfe4d8dbde2193a93c07769e
ep1h <ep1hxx@gmail.com>
2023-02-23 12:02:49 -0800

    Add common.h

---
+++ asmp-dll/src/game/types/common.h
@@ -0,0 +1,29 @@
+/**
+ * @file common.h
+ * @brief Helper macros for translating C++ call conventions to C.
+ */
+#ifndef TYPES_COMMON_H
+#define TYPES_COMMON_H
+
+#include <stdint.h>
+
+#define __hidden
+#define __cppobj
+
+#undef THIS_
+#define THIS_ ECX, void* EDX
+#define this  THIS_
+
+#ifndef __stdcall
+#define __stdcall __attribute__((__stdcall__))
+#endif /* __stdcall */
+
+#ifndef __fastcall
+#define __fastcall __attribute__((__fastcall__))
+#endif /* __fastcall */
+
+#define __THISCALL __attribute__((__fastcall__))
+#undef __thiscall
+#define __thiscall __THISCALL
+
+#endif /* TYPES_COMMON_H */

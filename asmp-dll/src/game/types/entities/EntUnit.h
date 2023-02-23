d3db2427f233d7057288446c19421a7793d01410
ep1h <ep1hxx@gmail.com>
2023-02-23 12:40:14 -0800

    Add EntUnit.h

---
+++ asmp-dll/src/game/types/entities/EntUnit.h
@@ -0,0 +1,18 @@
+#ifndef ENTITIES_ENTUNIT_H
+#define ENTITIES_ENTUNIT_H
+
+#include "../common.h"
+#include "EntObject.h"
+
+typedef struct __cppobj EntUnit
+{
+    EntObject ent_object;
+    uint32_t field_78;
+    uint32_t field_7C;
+    uint32_t field_80;
+    uint32_t field_84;
+    uint32_t field_88;
+    uint32_t field_8C;
+} EntUnit;
+
+#endif /* ENTITIES_ENTUNIT_H */

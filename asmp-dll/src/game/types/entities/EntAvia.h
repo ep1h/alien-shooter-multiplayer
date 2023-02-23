38bbdf0b6144b65140cf35cf5eecfa388d6526e2
ep1h <ep1hxx@gmail.com>
2023-02-23 12:43:46 -0800

    Add EntAvia.h

---
+++ asmp-dll/src/game/types/entities/EntAvia.h
@@ -0,0 +1,12 @@
+#ifndef ENTITIES_ENTAVIA_H
+#define ENTITIES_ENTAVIA_H
+
+#include "../common.h"
+#include "EntUnit.h"
+
+typedef struct __cppobj EntAvia
+{
+    EntUnit ent_unit;
+} EntAvia;
+
+#endif /* ENTITIES_ENTAVIA_H */

dbe68ca4c9c7f3cc1feb83889796e6608cec7499
ep1h <ep1hxx@gmail.com>
2023-02-23 12:04:38 -0800

    Add EntityItem.h

---
+++ asmp-dll/src/game/types/entities/EntityItem.h
@@ -0,0 +1,25 @@
+#ifndef ENTITIES_ENTITYITEM
+#define ENTITIES_ENTITYITEM
+
+#include "../common.h"
+
+typedef struct EntityItem
+{
+    float x;
+    float y;
+    float z;
+    uint32_t unk_0C;
+    uint32_t unk_10;
+    uint32_t unk_14;
+    uint32_t unk_18;
+    uint32_t unk_1C;
+    uint32_t unk_20;
+    uint32_t unk_24;
+    uint32_t unk_28;
+    uint32_t unk_2C;
+    uint32_t unk_30;
+    uint32_t unk_34;
+    uint32_t unk_38;
+} EntityItem;
+
+#endif /* ENTITIES_ENTITYITEM */

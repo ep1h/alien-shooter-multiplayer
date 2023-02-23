aa2115c059b28a5e4ecb62374c43c932e5dc31c8
ep1h <ep1hxx@gmail.com>
2023-02-23 12:16:58 -0800

    Add EntFrame.h

---
+++ asmp-dll/src/game/types/entities/EntFrame.h
@@ -0,0 +1,12 @@
+#ifndef ENTITIES_ENTFRAME_H
+#define ENTITIES_ENTFRAME_H
+
+#include "../common.h"
+#include "Entity.h"
+
+typedef struct __cppobj EntFrame
+{
+    Entity entity;
+} EntFrame;
+
+#endif /* ENTITIES_ENTFRAME_H */

d828d61b456c0dc74b80ba0a50bbf368a97549d4
ep1h <ep1hxx@gmail.com>
2023-02-23 13:06:30 -0800

    Add EntDepo.h

---
+++ asmp-dll/src/game/types/entities/EntDepo.h
@@ -0,0 +1,13 @@
+#ifndef ENTITIES_ENTDEPO_H
+#define ENTITIES_ENTDEPO_H
+
+#include "../common.h"
+#include "EntUnit.h"
+
+typedef struct __cppobj EntDepo
+{
+    EntUnit ent_unit;
+    uint32_t unk_0x90[254];
+} EntDepo;
+
+#endif /* ENTITIES_ENTDEPO_H */

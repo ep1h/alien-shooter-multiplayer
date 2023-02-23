e1f09c9c7f5e326189f2a606f98474610578fe8a
ep1h <ep1hxx@gmail.com>
2023-02-23 12:46:38 -0800

    Add EntBalloon.h

---
+++ asmp-dll/src/game/types/entities/EntBalloon.h
@@ -0,0 +1,12 @@
+#ifndef ENTITIES_ENTBALLOON_H
+#define ENTITIES_ENTBALLOON_H
+
+#include "../common.h"
+#include "EntAvia.h"
+
+typedef struct __cppobj EntBalloon
+{
+    EntAvia ent_avia;
+} EntBalloon;
+
+#endif /* ENTITIES_ENTBALLOON_H */

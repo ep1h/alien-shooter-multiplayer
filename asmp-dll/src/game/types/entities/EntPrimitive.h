e6d749981a69ae3e1f6257ceedbe46bf9e7dd3d7
ep1h <ep1hxx@gmail.com>
2023-02-23 12:05:43 -0800

    Add EntPrimitive.h

---
+++ asmp-dll/src/game/types/entities/EntPrimitive.h
@@ -0,0 +1,12 @@
+#ifndef ENTITIES_ENTPIMITIVE_H
+#define ENTITIES_ENTPIMITIVE_H
+
+#include "../common.h"
+#include "Entity.h"
+
+typedef struct __cppobj EntPrimitive
+{
+    Entity entity;
+} EntPrimitive;
+
+#endif /* ENTITIES_ENTPIMITIVE_H */

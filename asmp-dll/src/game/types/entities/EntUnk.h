b88f3e7f910750ff74855d161cb832d005e3c932
ep1h <ep1hxx@gmail.com>
2023-02-23 12:21:27 -0800

    Add EntUnk.h

---
+++ asmp-dll/src/game/types/entities/EntUnk.h
@@ -0,0 +1,12 @@
+#ifndef ENTITIES_ENTUNK_H
+#define ENTITIES_ENTUNK_H
+
+#include "../common.h"
+#include "Entity.h"
+
+typedef struct __cppobj EntUnk
+{
+    Entity entity;
+} EntUnk;
+
+#endif /* ENTITIES_ENTUNK_H */

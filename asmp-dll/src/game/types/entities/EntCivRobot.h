4509e89af6c6f208e47f588b17fba06d999a8d5b
ep1h <ep1hxx@gmail.com>
2023-02-23 13:01:54 -0800

    Add EntCivRobot.h

---
+++ asmp-dll/src/game/types/entities/EntCivRobot.h
@@ -0,0 +1,16 @@
+#ifndef ENTITIES_ENTCIVROBOT_H
+#define ENTITIES_ENTCIVROBOT_H
+
+#include "../common.h"
+#include "EntCreature.h"
+
+typedef struct __cppobj EntCivRobot
+{
+    EntCreature ent_creature;
+    uint32_t field_98;
+    uint32_t field_9C;
+    uint32_t field_A0;
+    uint32_t field_A4;
+} EntCivRobot;
+
+#endif /* ENTITIES_ENTCIVROBOT_H */

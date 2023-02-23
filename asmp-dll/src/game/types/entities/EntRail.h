86c380cb260764391c797e2723c85145cc72f1da
ep1h <ep1hxx@gmail.com>
2023-02-23 12:30:54 -0800

    Add EntRail.h

---
+++ asmp-dll/src/game/types/entities/EntRail.h
@@ -0,0 +1,14 @@
+#ifndef ENTITIES_ENTRAIL_H
+#define ENTITIES_ENTRAIL_H
+
+#include "../common.h"
+#include "EntObject.h"
+
+typedef struct __cppobj EntRail
+{
+    EntObject ent_object;
+    uint32_t field_78;
+    uint32_t field_7C;
+} EntRail;
+
+#endif /* ENTITIES_ENTRAIL_H */

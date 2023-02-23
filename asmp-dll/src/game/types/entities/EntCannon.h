b91271c458a0fe14fb919492e23866a34d3a5128
ep1h <ep1hxx@gmail.com>
2023-02-23 12:23:09 -0800

    Add EntCannon.h

---
+++ asmp-dll/src/game/types/entities/EntCannon.h
@@ -0,0 +1,13 @@
+#ifndef ENTITIES_ENTCANNON_H
+#define ENTITIES_ENTCANNON_H
+
+#include "../common.h"
+#include "Entity.h"
+
+typedef struct __cppobj EntCannon
+{
+    Entity entity;
+    uint32_t field_70;
+} EntCannon;
+
+#endif /* ENTITIES_ENTCANNON_H */

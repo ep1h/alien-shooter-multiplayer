79ab5727479edcd1781557395ca6e78d5d11a89e
ep1h <ep1hxx@gmail.com>
2023-02-23 12:53:51 -0800

    Add EntBuilding.h

---
+++ asmp-dll/src/game/types/entities/EntBuilding.h
@@ -0,0 +1,17 @@
+#ifndef ENTITIES_ENTBUILDING_H
+#define ENTITIES_ENTBUILDING_H
+
+#include "../common.h"
+#include "EntUnit.h"
+
+typedef struct __cppobj EntBuilding
+{
+    EntUnit ent_unit;
+    uint32_t field_90;
+    uint32_t field_94;
+    uint32_t field_98;
+    uint32_t field_9C;
+    uint32_t field_A0;
+} EntBuilding;
+
+#endif /* ENTITIES_ENTBUILDING_H */

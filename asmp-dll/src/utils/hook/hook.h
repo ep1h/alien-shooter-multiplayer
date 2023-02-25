2838c87169212f249696cae5a36bb6f5b54584ce
ep1h <ep1hxx@gmail.com>
2023-02-25 12:00:30 -0800

    Add component for hooking

---
+++ asmp-dll/src/utils/hook/hook.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_HOOK_HOOK_H
+#define UTILS_HOOK_HOOK_H
+
+#include <stdbool.h>
+
+bool hook_init(void);
+void* hook_set(void* function_address, void* hook_address, unsigned char size);
+void hook_unset(void* function_address);
+void hook_destroy(void);
+
+#endif /* UTILS_HOOK_HOOK_H */

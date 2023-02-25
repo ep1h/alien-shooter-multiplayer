c886860eed91d42ba9d6ba93d95694c41bfe7e4d
ep1h <ep1hxx@gmail.com>
2023-02-25 05:50:00 -0800

    Add Sound.h

---
+++ asmp-dll/src/game/types/Sound.h
@@ -0,0 +1,29 @@
+#ifndef TYPES_SOUND_H
+#define TYPES_SOUND_H
+
+#include "common.h"
+
+typedef struct Sound Sound;
+
+typedef float(__thiscall* Sound__set_sound_volume_t)(Sound* this, int volume);
+typedef float(__thiscall* Sound__set_music_volume_t)(Sound* this, int volume);
+
+typedef struct Sound
+{
+    uint32_t field_0[132];
+    void* lpDirectSound8;
+    uint32_t field_214[119];
+    uint32_t field_3F0;
+    uint32_t field_3F4;
+    uint32_t field_3F8;
+    uint32_t unk_volume;
+    uint32_t field_400;
+    uint32_t sound_volume;
+    uint32_t music_volume;
+    uint32_t field_40C;
+    uint32_t mb_loop;
+    uint32_t field_414;
+    uint32_t last_418;
+} Sound;
+
+#endif /* TYPES_SOUND_H */

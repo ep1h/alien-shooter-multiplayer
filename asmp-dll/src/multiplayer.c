a6bc30f5077e6d469621006aa81043e8647da590
ep1h <ep1hxx@gmail.com>
2023-03-01 13:01:02 -0800

    Add hook for load_menu function
    This hook is to replace default Alien Shooter menu with the game/AlienShooter/Maps/asmp_mainmenu.men.

--- asmp-dll/src/multiplayer.c
+++ asmp-dll/src/multiplayer.c
@@ -1,12 +1,46 @@
+#include <string.h>
 #include "multiplayer.h"
+#include "utils/hook/hook.h"
+#include "game/addresses.h"
+#include "game/game.h"
+#include "utils/console/console.h"
 
+static load_menu_t load_menu_tramp_ = 0;
+static char mainmenu_file_[] = "maps\\asmp_mainmenu.men";
+
+static int __stdcall _load_menu_hook(const char** menu_file);
+static bool set_hooks_(void);
+
+static int __stdcall _load_menu_hook(const char** menu_file)
+{
+    console_log("load_menu: %s\n", *menu_file);
+    if (strcmp(*menu_file, "maps\\mainmenu.men") == 0)
+    {
+        char* p = mainmenu_file_;
+        return load_menu_tramp_((const char**)&p);
+    }
+    return load_menu_tramp_(menu_file);
+}
+
+static bool set_hooks_(void)
+{
+    load_menu_tramp_ = hook_set((void*)FUNC_LOAD_MENU, _load_menu_hook, 8);
+    if (load_menu_tramp_)
+    {
+        return true;
+    }
+    return false;
+}
 
 bool multiplayer_init(void)
 {
-    return true;
+    if (set_hooks_())
+    {
+        return true;
+    }
+    return false;
 }
 
 void multiplayer_destroy(void)
 {
-
 }

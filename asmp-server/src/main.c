f83a7cefa2433a18b2c1154ad89916fe179133c8
ep1h <ep1hxx@gmail.com>
2023-03-06 08:39:16 -0800

    Implement top level server
    For now it's just a wrapper over low level server.

--- asmp-server/src/main.c
+++ asmp-server/src/main.c
@@ -1,11 +1,19 @@
+#include <windows.h>
 #include "server.h"
 
+#include "net/server/server.h"
 #ifndef EZTEST
 
 int main(int argc, char* argv[])
 {
     (void)argc;
     (void)argv;
+    NetServer* s = net_server_create(8888, 4, 5000, 5000);
+    while (s)
+    {
+        Sleep(10000);
+    }
+    net_server_destroy(s);
     return 0;
 }
 
